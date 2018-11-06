a13ac5e66390cb06f78065f0a88f202fe63a6a79
Projeto-LinuxUFPA <44652251+Projeto-LinuxUFPA@users.noreply.github.com>
2018-11-06 16:33:19 -0300

    Update windows_1252.py

--- Portal UFPA/windows_1252.py
+++ Portal UFPA/windows_1252.py
@@ -1,3 +1,19 @@
+#This file is part of Portal UFPA.
+
+#Foobar is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#Foobar is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
+
 def restore_windows_1252_characters(s):
     """Replace C1 control characters in the Unicode string s by the
     characters at the corresponding code points in Windows-1252,
